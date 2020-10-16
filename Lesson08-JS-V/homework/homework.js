f25e5ec01129400d92809f52cb1ebcc8057dab60
Kyle Larson <kylel@email.arizona.edu>
2020-10-16 15:31:15 -0700

    updated homework.js and added Feynman_Writing_Prompts.txt

--- Lesson08-JS-V/homework/homework.js
+++ Lesson08-JS-V/homework/homework.js
@@ -7,11 +7,24 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the class
+	function user(username, name, email){
+		this.username = username;
+		this.name = name;
+		this.email = email;
+		this.sayHi = function(){
+			var greeting = 'Hello, my name is ' + this.name;
+			return greeting;
+		};
+	}
+	return user('test','testtommy', 'testtommy@test.com');
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+	Constructor.prototype.sayHi = function(){
+		return 'Hello World!';
+	};
 }
 
 function addReverseString() {
@@ -19,6 +32,16 @@ function addReverseString() {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+	function String(){
+		arguments.prototype.reverse = function(){
+			var reversed = '';
+			for (let i=0; i<this.length; i++){
+				reversed.unshift(this[i]);
+			}
+			return reversed;
+		};
+	}
+	return (arguments+String(arguments));
 }
 
 // Do not modify code below this line.
