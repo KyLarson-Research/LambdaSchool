fddc0a2fdaf5df87c66e4611883797668b38d8f3
Kyle Larson <kylel@email.arizona.edu>
2020-10-19 19:00:42 -0700

    added Feynman...txt and updated homework.js with my functions and code

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,11 +6,28 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+	function fact(x){
+		if(x===0){return 1;}
+		return x*fact(x-1);
+	}
+	return fact(n);
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+	/*function wrapValue(t){
+		let local = t;
+		return () => local;
+	}*/
+	function fib(j){
+		if(j < 0) {return 0;}
+		if(j === 0) {return 1;}
+		if(j > 0) {
+			var sum = fib(j-1)+fib(j-2);
+			return sum;
+		}
+	}
 }
 
 // Do not modify code below this line.
